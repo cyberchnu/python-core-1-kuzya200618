fd27163a81f9ea976ed7bc0138c0dbc136673369
Davyd Kuzovenkov <davidkuzovenkov@gmail.com>
2024-05-27 14:53:16 +0300

    taksss

--- src/calculate_fuel.py
+++ src/calculate_fuel.py
@@ -1,6 +1,13 @@
 def calculate_fuel(distance):
-  def calculate_fuel(distance):
-    fuel = distance * 10  # Обсяг палива - 10 разів більше відстані
-    if fuel < 100:  # Перевіряємо, чи не менше за 100 літрів
-        return 100
-    return fuel
+    # Припустимо, що витрати палива становлять 10 одиниць палива на 1 одиницю відстані.
+    fuel_needed = distance * 10
+    return fuel_needed
+
+def test_calculate_fuel1():
+    assert calculate_fuel(15) == 150
+
+def test_calculate_fuel2():
+    assert calculate_fuel(0) == 0
+
+def test_calculate_fuel3():
+    assert calculate_fuel(5) == 50
