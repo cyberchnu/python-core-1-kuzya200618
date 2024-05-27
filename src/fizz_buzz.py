6e2198ed4e38ce2b53b326c9d4bc63e1d292a022
Davyd Kuzovenkov <davidkuzovenkov@gmail.com>
2024-05-27 16:07:11 +0300

    All tasks

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,9 +1,9 @@
 def fizz_buzz(param):
-    if param % 3 == 0 and param % 5 == 0:
-        return "fizzbuzz"
-    elif param % 3 == 0:
-        return "fizz"
-    elif param % 5 == 0:
-        return "buzz"
+    if param % 3 == 0 and param % 5 != 0:
+        return "Fizz"
+    elif param % 3 != 0 and param % 5 == 0:
+        return "Buzz"
+    elif param % 3 == 0 and param % 5 == 0:
+        return "FizzBuzz"
     else:
-        return param
+        return str(param)
